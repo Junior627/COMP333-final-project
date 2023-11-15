2c63015094ffb8ebfa1527e37aab1e2c8e23f032
WesGitAccKL <kliao@wesleyan.edu>
2023-11-15 13:12:20 -0500

    created framework for ship movement game state

---
+++ GameStates/shipgame.py
@@ -0,0 +1,19 @@
+import pygame
+from .generic_state import generic_state
+
+'''Skeleton code for the game state upon level selection.
+'''
+
+class shipgame(generic_state):
+    def __init__(self):
+        super(shipgame, self).__init__()
+    
+    def get_event(self, event):
+        '''Placeholder code for handling events
+        '''
+        pass
+
+    def draw(self, surface):
+        '''Placeholder code for screen display
+        '''
+        pass
