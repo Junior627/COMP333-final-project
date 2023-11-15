08b98c1a241680838058ba1121264adad49924d9
WesGitAccKL <kliao@wesleyan.edu>
2023-11-15 13:17:40 -0500

    incorporated gamestate framework into main project file

--- main.py
+++ main.py
@@ -3,12 +3,28 @@ import pygame
 
 import constants
 
+from gameloop import Game
+from GameStates.menu import menu
+from GameStates.levels import levels
+from GameStates.customization import customization
+from GameStates.shipgame import shipgame
+
 # initialize pygame
 pygame.init()
 
 # create game window
 screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
 pygame.display.set_caption("The Aurora Armada")
+
+states = {
+    "menu": menu(),
+    "levels": levels(),
+    "customization": customization(),
+    "shipgame": shipgame()
+}
+
+gameplay = Game(screen, states, "menu")
+gameplay.run()
 
 pygame.quit()
 sys.exit()
