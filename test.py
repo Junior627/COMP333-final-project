fe5b26bdc61febdfc1879d4207bcd84cd328a618
Junior <junior62725@gmail.com>
2023-12-05 20:41:19 -0500

    added more test code

--- test.py
+++ test.py
@@ -13,15 +13,21 @@ screen = pygame.display.set_mode(size)
 
 player = Player()
 enemyList = []
+
+
 x = 0
-while x < 10:
-    enemyList.append(Shooter((SCREEN_WIDTH / 10) * x, 10))
+while x < 6:
+    enemyList.append(Shooter((SCREEN_WIDTH / 6) * x, 10))
     x +=1
     
 y = 0
-while y < 4:
-    enemyList.append(Bomber((SCREEN_WIDTH / 4) * (y + .5) , 30))
+while y < 3:
+    enemyList.append(Bomber((SCREEN_WIDTH / 3) * (y + .33) , 50))
     y+=1
+z = 0
+while z < 4:
+    enemyList.append(Chaser((SCREEN_WIDTH / 4) * (z + .5) , 90))
+    z+=1
 
 clock=  time.Clock()
 bulletManager = BulletManager()
@@ -29,7 +35,7 @@ fxManager = visualFXManager()
 
 def main():
     while True:
-        screen.fill((0,0,0))
+        screen.fill((0,0,255))
         for event in pygame.event.get():
             if event.type == pygame.QUIT: sys.exit()
         dir = Vector2(0,0)
@@ -59,23 +65,28 @@ def main():
                     bulletManager.add_enemy_bullet(enemy.shootBullet(Vector2(player.entity_collider.centerx,player.pos.y)))
                 elif (enemy.bullet_cooldown == 0 and enemy.__class__.__name__ == "Bomber"):
                     bulletManager.add_enemy_bomb(enemy.shootBomb(Vector2(player.entity_collider.centerx , player.pos.y)))
-                screen.blit(enemy.entity, enemy.pos)
+                elif(enemy.__class__.__name__ == "Chaser"):
+                    if(enemy.movement_cooldown == 0):
+                        enemy.createPath(Vector2(player.entity_collider.centerx,player.pos.y))
+                screen.blit(enemy.entity, enemy.entity_collider)
                 
         screen.blit(player.entity, player.pos)
         
-        
-        bulletManager.update_bullets()                    
+                           
         for bullet in bulletManager.player_bullets:
             screen.blit(bullet.bullet , bullet.pos)
         for bullet in bulletManager.enemy_bullets:
             screen.blit(bullet.bullet , bullet.pos)
+            if bullet.out_of_bounds():
+                fxManager.add_explosion_fx(bullet.pos.x,bullet.pos.y, BulletExplosion)
         for bomb in bulletManager.enemy_bombs:
             screen.blit(bomb.bomb , bomb.pos)
-            
+        bulletManager.update_bullets()     
         enemy_to_remove = []
+        
         for enemy in enemyList:
             if bulletManager.check_for_collision(bulletManager.player_bullets,enemy):
-                fxManager.add_explosion_fx(enemy.pos.x , enemy.pos.y)
+                fxManager.add_explosion_fx(enemy.pos.x , enemy.pos.y, Explosion)
                 enemy_to_remove.append(enemy)
         
         for enemy in enemy_to_remove:
@@ -83,12 +94,13 @@ def main():
         
         fxManager.update_lists()
         for explosion in fxManager.explosion_list:
-            screen.blit(explosion.explode[explosion.anim_index], explosion.pos)
-            
+            screen.blit(explosion.explode[explosion.anim_index], explosion.rect)
+
+                            
         player.update(dir)
         
         if bulletManager.check_for_collision(bulletManager.enemy_bullets, player):
-            print("HIT!")
+            fxManager.add_explosion_fx(bullet.pos.x,bullet.pos.y, BulletExplosion)
         
         pygame.display.update()
         clock.tick(60)
