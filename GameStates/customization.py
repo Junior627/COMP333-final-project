ba7d03c5cdf8e85134b7e28f786561c8f52cea44
WesGitAccKL <kliao@wesleyan.edu>
2023-12-07 03:55:26 -0500

    added unit tests for level selection

--- GameStates/customization.py
+++ GameStates/customization.py
@@ -13,7 +13,7 @@ current_choices- a list of the indices of the current components the user has se
 position- the current option the user is hovering over within the current menu.
 '''
 
-class Customization(generic_state):
+class customization(generic_state):
     def __init__(self):
         super(customization, self).__init__()
         self.current_engine = 0
