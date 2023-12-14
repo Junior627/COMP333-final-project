511d36f237e2976f7f9f4b4a87fcd486d7c467af
WesGitAccKL <kliao@wesleyan.edu>
2023-12-13 23:33:07 -0500

    added docstrings for visualFXManager.py

--- visualFXManager.py
+++ visualFXManager.py
@@ -1,6 +1,10 @@
 from pygame import *
 
-
+'''Code used to manage visual effects within the game.
+Specific attributes:
+explosion_list- a list of all current explosions.
+bullet_explosion_list- a list of all current bullet explosions.
+'''
 
 class visualFXManager:
     def __init__(self):
@@ -8,18 +12,37 @@ class visualFXManager:
         self.bullet_explosion_list = []
         
     def update_lists(self):
+        '''Updates the list of explosions.
+        '''
         self.update_fx_list(self.explosion_list)
         
     def update_fx_list(self, list):
-        
+        '''Updates a given list of visual effects.
+        Args: fx_list- a list of visual effects to update.
+        '''
         for fx in list:
             fx.draw()
             if fx.endOfAnim():
                 list.remove(fx)
                 
     def add_explosion_fx(self, x , y, fx_arg):
+        '''Adds an explosion effect to the list of explosion effects.
+        Args: x- the x coordinate of the explosion.
+        y- the y coordinate of the explosion.
+        fx_arg- the type of explosion effect to add.
+        '''
         self.explosion_list.append(fx_arg(x,y))
     
+
+'''An instance of a ship explosion visual effect.
+Specific attributes:
+pos- the position of the explosion.
+anim_index- the index of the current sprite corresponding to the
+current frame of the explosion.
+explode- a list containing all sprites of the explosion.
+rect- the dimensions of the explosion effect.
+anim_counter- the current frame of the explosion.
+'''
     
 class Explosion:
     def __init__(self, x,y):
@@ -37,16 +60,32 @@ class Explosion:
         self.anim_counter = 0
         
     def endOfAnim(self):
+        '''Checks if the animation has finished.
+        Returns: true if the animation has finished, false otherwise
+        '''
         if self.anim_index >= len(self.explode):
             return True
         return False
     def draw(self):
+        '''Updates the required attributes to advance the explosion to its next frame.
+        '''
         self.rect.center = (self.pos.x , self.pos.y) 
         self.anim_counter +=1       
         self.anim_index = (self.anim_counter + 1) // 3
     
     def __del__(self):
         pass
+
+
+'''An instance of a vullet explosion visual effect.
+Specific attributes:
+pos- the position of the explosion.
+anim_index- the index of the current sprite corresponding to the
+current frame of the explosion.
+explode- a list containing all sprites of the explosion.
+rect- the dimensions of the explosion effect.
+anim_counter- the current frame of the explosion.
+'''
 
 class BulletExplosion:
     def __init__(self, x ,y):
@@ -62,10 +101,15 @@ class BulletExplosion:
         self.rect = Rect(x,y,self.explode[0].get_width() , self.explode[0].get_height())
         self.anim_counter = 0
     def endOfAnim(self):
+        '''Checks if the animation has finished.
+        Returns: true if the animation has finished, false otherwise
+        '''
         if self.anim_index >= len(self.explode):
             return True
         return False
     def draw(self):
+        '''Updates the required attributes to advance the explosion to its next frame.
+        '''
         self.rect.center = (self.pos.x , self.pos.y) 
         self.anim_counter +=1       
         self.anim_index = (self.anim_counter + 1) // 3
