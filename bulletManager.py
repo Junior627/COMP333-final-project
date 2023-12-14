5ae7e9cabda75038993e33732dc0c2529aa17b6e
WesGitAccKL <kliao@wesleyan.edu>
2023-12-13 23:51:18 -0500

    removed testing print statement in bulletManager

--- bulletManager.py
+++ bulletManager.py
@@ -56,7 +56,6 @@ class BulletManager:
 
         for bullet in bulletList:
             if bullet.bullet_collider.colliderect(target.entity_collider):
-                print('Collision detected!')
                 bullets_to_remove.append(bullet)
                 if_collide = True
         # Remove bullets outside the loop
